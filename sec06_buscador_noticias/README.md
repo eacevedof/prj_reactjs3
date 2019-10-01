3b0d3948b221e4811df073e57a17bdf1c26d239a
Eduardo A.F <eacevedof@yahoo.es>
2019-10-01 21:51:12 +0200

    sec06 7. Cargando las imágenes condicionalmente

--- sec06_buscador_noticias/README.md
+++ sec06_buscador_noticias/README.md
@@ -130,8 +130,23 @@ const Noticia = ({noticia}) => {
   )
 ```
 ## 7. Cargando las imágenes condicionalmente
-- 
 ```js
+//condicionalmente cargar la imagen si esta disponible
+const imagen = (urlToImage)
+        ?<div className="card-image">
+          <img src={urlToImage} alt={title}/>
+          <span className="card-title">{source.name}</span>
+        </div>
+        :null
+
+  return(  
+    <div className="col s12 m6 l4">
+      <div className="card">
+        
+        {imagen}
+        
+        <div className="card-content">
+          <h3>{title}</h3>        
 ```
 ## 8. Creando un Buscador de Categorias
 - 
--- sec06_buscador_noticias/noticiasreact/src/components/Noticia.js
+++ sec06_buscador_noticias/noticiasreact/src/components/Noticia.js
@@ -6,13 +6,19 @@ const Noticia = ({noticia}) => {
   console.log("obj noticia: ",noticia)
   //extraer los datos
   const { urlToImage, url,title,description,source} = noticia
+
+  //condicionalmente cargar la imagen si esta disponible
+  const imagen = (urlToImage)
+        ?<div className="card-image">
+          <img src={urlToImage} alt={title}/>
+          <span className="card-title">{source.name}</span>
+        </div>
+        :null
+
   return(  
     <div className="col s12 m6 l4">
       <div className="card">
-        <div className="card-image">
-          <img src={urlToImage} alt={title}/>
-        </div>
-
+        {imagen}
         <div className="card-content">
           <h3>{title}</h3>
           <p>{description}</p>
