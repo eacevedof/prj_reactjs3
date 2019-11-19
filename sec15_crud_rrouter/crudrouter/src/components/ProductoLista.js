de9b2bcfb890027ed8e4c7c2d7009bc347ae7236
Eduardo A.F <eacevedof@yahoo.es>
2019-11-19 22:03:59 +0100

    sec15 10. Mostrando los Productos

--- sec15_crud_rrouter/crudrouter/src/components/ProductoLista.js
+++ sec15_crud_rrouter/crudrouter/src/components/ProductoLista.js
@@ -1,9 +1,40 @@
 //ProductoLista.js
 import React from 'react';
+import {Link} from "react-router-dom"
 
+//siempre que viene props.producto se puede hacer esto
 function ProductoLista({producto}){
+
+  const eliminarProducto = id => {
+    console.log("eliminando",id)
+  }
+
   return (
-    <div>ProductoLista</div>
+    <li 
+      className="list-group-item d-flex justify-content-between align-items-center"
+      data-categoria={producto.categoria}
+    >
+      <p>
+        {producto.nombrePlatillo}{' '}
+        <span className="font-weight-bold">${producto.precioPlatillo}</span>
+      </p>
+
+      <div>
+        <Link
+          to={`/productos/editar/${producto.id}`}
+          className="btn btn-success mr-2"
+        >Editar</Link>
+
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={()=>eliminarProducto(producto.id)}
+        >
+          Eliminar &times;
+        </button>
+
+      </div>
+    </li>
   )
 
 }
