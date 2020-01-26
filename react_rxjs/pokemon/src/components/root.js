2c241e03cd36d87a36135b84c03effe278b6e7a7
ioedumac <eacevedof@yahoo.es>
2020-01-26 16:48:11 +0100

    pokemon ini

---
+++ react_rxjs/pokemon/src/components/root.js
@@ -0,0 +1,13 @@
+//src/components/root.js
+import React, { Component } from 'react';
+
+export default class Root extends Component {
+  state = {}
+  render(){
+    return (
+      <>
+        This is the Root Component. Your welcome!
+      </>
+    )
+  }
+}
---
+++ react_rxjs/pokemon/src/index.js
@@ -0,0 +1,9 @@
+//src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from "./components/root"
+
+const nodes = (
+  <Root/>
+)
+ReactDOM.render(nodes, document.getElementById("root"))
