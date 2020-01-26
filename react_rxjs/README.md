aa52b4816c3b41b00c104f4a4995cc94c2d07cad
ioedumac <eacevedof@yahoo.es>
2020-01-26 18:27:06 +0100

    foto con behavioursubject

--- react_rxjs/README.md
+++ react_rxjs/README.md
@@ -142,3 +142,58 @@ Search for the keywords to learn more about each error.
 This error occurred during the build time and cannot be dismissed
 ```
 ### [Ejemplo con pokemon API](https://youtu.be/Urv82SGIu_0?t=770)
+- https://youtu.be/Urv82SGIu_0?t=1022
+- Esto funciona sin utilizar get_async_pokemon_name solo con behavioursubject
+```js
+//src/components/rxjs1.js
+import React, { useState, useEffect } from 'react';
+import {from, BehaviorSubject} from "rxjs"
+import {map, filter, delay, mergeMap} from "rxjs/operators"
+
+const get_async_pokemon_name = async name => {
+  const {results: allPokemons} = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1000").then(result => result.json)
+  return allPokemons.filter(pokemon => pokemon.name.includes(name))
+}
+
+const objsubject = new BehaviorSubject("")
+
+function Rxjs1() {
+  const [search, set_search] = useState("")
+  const [results, set_results] = useState([])
+
+  useEffect(()=>{
+    console.log("useEffect subscribe")
+    const objsuscription = objsubject.subscribe( r => {
+      console.log("observer called r:",r);
+      set_results(r)
+    })
+
+    console.log("calling objsuscription.unsubscribe")
+    return () => objsuscription.unsubscribe()
+
+  },[])//useEffect
+
+  const on_change = e => {
+    const strnew = e.target.value
+    set_search(strnew)
+    objsubject.next(strnew)
+  }
+
+  return (
+    <>
+      <input 
+        type="text" 
+        placeholder="Search" 
+        value={search} 
+        onChange={on_change}
+      />
+      <div>
+        {JSON.stringify(results, null, 2)}
+      </div>
+    </>
+  )
+
+}//function Rxjs1 (hook)
+
+export default Rxjs1
+```
--- react_rxjs/pokemon/src/components/rxjs1.js
+++ react_rxjs/pokemon/src/components/rxjs1.js
@@ -1,27 +1,24 @@
 //src/components/rxjs1.js
 import React, { useState, useEffect } from 'react';
-import {from} from "rxjs"
+import {from, BehaviorSubject} from "rxjs"
 import {map, filter, delay, mergeMap} from "rxjs/operators"
 
-const objnumbers$ = from([1,2,3,4,5])
+const get_async_pokemon_name = async name => {
+  const {results: allPokemons} = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=1000").then(result => result.json)
+  return allPokemons.filter(pokemon => pokemon.name.includes(name))
+}
 
-const objsquared$ = objnumbers$.pipe(
-  filter(n => n > 2),
-  mergeMap(n => from([n]).pipe(delay(1000 * n))),
-  map(n => n * n)
-)
-
+const objsubject = new BehaviorSubject("")
 
 function Rxjs1() {
-
-  const [currnumber, set_currnumber] = useState(0)
+  const [search, set_search] = useState("")
+  const [results, set_results] = useState([])
 
   useEffect(()=>{
     console.log("useEffect subscribe")
-    const objsuscription = objsquared$.subscribe( r => {
+    const objsuscription = objsubject.subscribe( r => {
       console.log("observer called r:",r);
-      set_currnumber(r)
-      //this.objsuscription.unsubscribe()
+      set_results(r)
     })
 
     console.log("calling objsuscription.unsubscribe")
@@ -29,10 +26,23 @@ function Rxjs1() {
 
   },[])//useEffect
 
+  const on_change = e => {
+    const strnew = e.target.value
+    set_search(strnew)
+    objsubject.next(strnew)
+  }
 
   return (
     <>
-      Current number is {currnumber}
+      <input 
+        type="text" 
+        placeholder="Search" 
+        value={search} 
+        onChange={on_change}
+      />
+      <div>
+        {JSON.stringify(results, null, 2)}
+      </div>
     </>
   )
 
