2984d3e1186b4b6c8c48aa8086065c0d203b5452
ioedumac <eacevedof@yahoo.es>
2020-01-26 17:06:09 +0100

    primer observer configurado

--- react_rxjs/pokemon/src/components/rxjs1.js
+++ react_rxjs/pokemon/src/components/rxjs1.js
@@ -1,4 +1,18 @@
+//src/components/rxjs1.js
 import React, { Component } from 'react';
+import {from} from "rxjs"
+import {map} from "rxjs/operators"
+
+let objnumbers$ = from([1,2,3,4,5])
+let objsquared$ = objnumbers$.pipe(
+  map(val => val * val)
+)
+
+//observer
+const fn_next = r => console.log("observer called","r:",r)
+
+//observer a la escucha del array de numeros
+objsquared$.subscribe(fn_next)
 
 class Rxjs1 extends Component {
   state = {}
