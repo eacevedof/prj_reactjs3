df17e5b8499c742a59581338eab0e9b8370182ba
ioedumac <eacevedof@yahoo.es>
2020-01-26 19:54:42 +0100

    poniendo trazas para entender subject

--- react_rxjs/pokemon/src/components/rxjs1.js
+++ react_rxjs/pokemon/src/components/rxjs1.js
@@ -1,7 +1,7 @@
 //src/components/rxjs1.js
 import React, { useState, useEffect } from 'react';
 import {from, BehaviorSubject} from "rxjs"
-import {map, filter, delay, mergeMap, debounceTime, distinctUntilChanged} from "rxjs/operators"
+import {map, filter, delay, mergeMap, debounceTime, distinctUntilChanged, tap} from "rxjs/operators"
 
 const get_async_pokemon_name = async name => {
   console.log("get_async_pokemon_name.name",name)
@@ -12,34 +12,46 @@ const get_async_pokemon_name = async name => {
   return allpokemons.filter(pokemon => pokemon.name.includes(name))
 }
 
-const objsubject = new BehaviorSubject("")
-const result$ = objsubject.pipe(
+//es observable y observer a la vez
+//tiene un estado inicial que es de tipo singleton, es decir, cada vez que modifica su estado
+//lo comparte a todos los observadores
+const behavsubj$ = new BehaviorSubject("")
+
+const async$ = behavsubj$.pipe(
+  tap("behavsubj$ ini str:"),
   filter(str => str.length>0),
   debounceTime(750),
   distinctUntilChanged(),
-  mergeMap(str => from(get_async_pokemon_name(str)))
+  mergeMap(str => from(get_async_pokemon_name(str))),
+  tap("behavsubj$ end str:"),
 )
+//async$ es annonymousSubject
+console.log("type of async$:",typeof async$,async$)
+console.log("behavsubj$ y async$ configurados...")
 
 function Rxjs1() {
+  console.log("starting Rxjs1()")
   const [search, set_search] = useState("")
+  //set_results observará a async$
   const [results, set_results] = useState([])
 
   useEffect(()=>{
-    console.log("useEffect subscribe")
-    const objsuscription = result$.subscribe( r => {
-      console.log("observer called r:",r);
+    console.log("useEffect() suscribing to async$")
+    const subscriber = async$.subscribe( r => {
       set_results(r)
     })
 
-    console.log("calling objsuscription.unsubscribe")
-    return () => objsuscription.unsubscribe()
+    console.log("unsubscribing from async$",subscriber)
+    return () => subscriber.unsubscribe()
 
   },[])//useEffect
 
   const on_change = e => {
     const strnew = e.target.value
     set_search(strnew)
-    objsubject.next(strnew)
+    //aqui actua como observador
+    console.log("llmamando a behavsubj$.next() con strnew",strnew)
+    behavsubj$.next(strnew)
   }
 
   return (
