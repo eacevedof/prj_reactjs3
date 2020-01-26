7b87ceb8abada58f42de29de2ebb10e6658d36c5
ioedumac <eacevedof@yahoo.es>
2020-01-26 16:59:22 +0100

    rxjs1

--- react_rxjs/pokemon/src/components/root.js
+++ react_rxjs/pokemon/src/components/root.js
@@ -2,12 +2,14 @@
 import React, { Component } from 'react';
 import "../style.css"
 
+import Rxjs1 from "./rxjs1"
+
 export default class Root extends Component {
   state = {}
   render(){
     return (
       <>
-        This is the Root Component. Your welcome!
+        <Rxjs1 />
       </>
     )
   }
---
+++ react_rxjs/pokemon/src/components/rxjs1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Rxjs1 extends Component {
+  state = {}
+  render(){
+    return (
+      <>
+        Rxjs1
+      </>
+    )
+  }
+}
+
+export default Rxjs1
