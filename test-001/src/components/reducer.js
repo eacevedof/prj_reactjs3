b673de1625d47cbabe0085250011e86cc252c5de
ioedumac <eacevedof@yahoo.es>
2021-07-16 17:30:20 +0200

    ya carga con reducer

--- test-001/src/components/reducer.js
+++ test-001/src/components/reducer.js
@@ -6,19 +6,24 @@ const stateproducts = {
   products: []
 }
 
+const ACTIONS = {
+  FETCH_SUCCESS: "FETCH_SUCCESS",
+  FETCH_ERROR: "FETCH_ERROR"
+}
+
 const fn_reducer = (state, action) => {
   switch(action.type) {
-    case "FECTH_SUCCESS":
+    case ACTIONS.FETCH_SUCCESS:
       return {
         error: "",
         loading: false,
-        product: action.payload,
+        products: action.payload,
       }
-    case "FECTH_ERROR":
+    case ACTIONS.FETCH_ERROR:
       return {
         error: "bad request",
         loading: false,
-        product: action.payload 
+        products: action.payload 
       }
     default:
       return state
@@ -29,31 +34,31 @@ function Reducer(props) {
   const [state, dispatch] = useReducer(fn_reducer, stateproducts)
 
   useEffect(() => {
+    //dispatch({type: ""})
+    
     const url = "https://json.theframework.es/index.php?getfile=app_product.json"
     fetch(url)
-      .then(response => {
-        const data = response.json()
-        console.log(data)
-        return data
-      })
+      .then(response => response.json())
+      //.then(data => console.table(data))
       .then(data => dispatch({
-        type: "FETCH_SUCCESS",
+        type: ACTIONS.FETCH_SUCCESS,
+        error: false,
         payload: data
       }))
       .catch(error => dispatch({
-        type: "FETCH_ERROR",
-        payload: []
+        type: ACTIONS.FETCH_ERROR
       }))
 
   },[])
 
   return (
     <>
+      {state.error && <h1>Error</h1>}
       <ul>
-      {stateproducts.loading ? 
+      {state.loading ? 
         "Loading" 
         : 
-        stateproducts.products.map(product => <li key={product.id}> {product.id} - {product.description} </li>)
+        state.products.map(product => <li key={product.id}> {product.id} - {product.description} </li>)
       }
       </ul>
     </>
