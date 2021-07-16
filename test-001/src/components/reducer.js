4bb0974d6ccc72f9e280046f0ece0a0545cb3d0f
ioedumac <eacevedof@yahoo.es>
2021-07-16 20:03:37 +0200

    reducer con b5

--- test-001/src/components/reducer.js
+++ test-001/src/components/reducer.js
@@ -1,14 +1,17 @@
 import { useReducer, useEffect } from "react"
+import "../css/reducer.css"
+
+const URL_PRODUCTS = "https://json.theframework.es/index.php?getfile=app_product.json"
+
+const ACTIONS = {
+  FETCH_SUCCESS: "FETCH_SUCCESS",
+  FETCH_ERROR: "FETCH_ERROR"
+}
 
 const stateproducts = {
   loading: true,
   error: "",
   products: []
-}
-
-const ACTIONS = {
-  FETCH_SUCCESS: "FETCH_SUCCESS",
-  FETCH_ERROR: "FETCH_ERROR"
 }
 
 const fn_reducer = (state, action) => {
@@ -30,37 +33,74 @@ const fn_reducer = (state, action) => {
   }
 }
 
-function Reducer(props) {
+
+function Reducer() {
   const [state, dispatch] = useReducer(fn_reducer, stateproducts)
 
-  useEffect(() => {
-    //dispatch({type: ""})
-    
-    const url = "https://json.theframework.es/index.php?getfile=app_product.json"
-    fetch(url)
+  useEffect(() => {  
+    fetch(URL_PRODUCTS)
       .then(response => response.json())
-      //.then(data => console.table(data))
       .then(data => dispatch({
         type: ACTIONS.FETCH_SUCCESS,
         error: false,
         payload: data
       }))
       .catch(error => dispatch({
-        type: ACTIONS.FETCH_ERROR
+        type: ACTIONS.FETCH_ERROR,
+        error: true,
+        payload: []
       }))
+  }, [])
 
+  useEffect(()=>{
+    (async () => {
+        try {
+          const response = await fetch(URL_PRODUCTS)
+          if( !response.ok ) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+          }
+          const data = await response.json()
+          dispatch({
+            type: ACTIONS.FETCH_SUCCESS,
+            error: false,
+            payload: data
+          })
+
+        }
+        catch (error) {
+          console.log(error)
+          dispatch({
+            error: true,
+            type: ACTIONS.FETCH_ERROR,
+            payload: []
+          })          
+        }
+        
+      })()
   },[])
 
   return (
     <>
-      {state.error && <h1>Error</h1>}
-      <ul>
-      {state.loading ? 
-        "Loading" 
-        : 
-        state.products.map(product => <li key={product.id}> {product.id} - {product.description} </li>)
-      }
-      </ul>
+      <div className="grid">
+        <div className="center">
+            {state.error && <h1 className="badge bg-danger">Error</h1>}
+            <ul className="list-group">
+            {state.loading ? 
+              "Loading" 
+              : 
+              state.products.map(product =>         
+                <li key={product.id} className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                  <div className="fw-bold">{product.name}</div>
+                    {product.description}
+                  </div>
+                  <span className="badge bg-primary rounded-pill">{product.id}</span>
+              </li>          
+              )
+            }
+            </ul>
+          </div>
+      </div>
     </>
   )
 }
